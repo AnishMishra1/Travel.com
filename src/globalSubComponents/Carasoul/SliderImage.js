6b06f790badaeadd88258b38f30f7f50357bc361
Sachin <sachinkaurav65@gmail.com>
2024-02-24 16:27:16 +0530

    add call

--- src/globalSubComponents/Carasoul/SliderImage.js
+++ src/globalSubComponents/Carasoul/SliderImage.js
@@ -12,6 +12,6 @@ export default [
   {
     title: "ACT NOW!",
     description: '"Avail Awesome Discounts !!"',
-    urls: "https://img.freepik.com/free-photo/airport-terminal_1417-1462.jpg?t=st=1708767020~exp=1708770620~hmac=ab357479e0978678f9deef9ffafea7969aac9ee02b1f30edf7e56bc8b2ca920c&w=900",
+    urls: "https://img.freepik.com/free-photo/shooting-from-airplane-window_169016-1852.jpg?t=st=1708772182~exp=1708775782~hmac=4662ec73c4bebdb1c11f4d7742b1eaa1c18232071b8ae4291dc5f1453c5bc5ae&w=996",
   },
 ];
